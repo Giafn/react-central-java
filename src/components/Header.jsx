4db0a9ade50278497afad40e3ca277676bf1e418
Giafn <giafauzan11@gmail.com>
2024-12-10 22:19:51 +0700

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,6 +23,7 @@ const Header = () => {
     }
   };
 
+  if (localStorage.getItem("token")) {
   return (
     <header className="bg-[#C62E2E] text-white p-4 flex justify-between items-center fixed top-0 w-full z-10 border-b-2 border-gray-300">
       {/* Logo */}
@@ -63,6 +64,49 @@ const Header = () => {
       </nav>
     </header>
   );
+  } else {
+    return (
+      <header className="bg-[#C62E2E] text-white p-4 flex justify-between items-center fixed top-0 w-full z-10 border-b-2 border-gray-300">
+        {/* Logo */}
+        <div className="logo text-xl font-bold font-ribeye flex-shrink-0 pl-5 flex flex-col justify-center items-center">
+          <span>CENTRAL</span>
+          <span>JAVA</span>
+        </div>
+  
+        {/* Navigasi */}
+        <nav className="nav flex space-x-4 items-center ml-20">
+          <Link to="/home" className="">
+            Home
+          </Link>
+          <CategoryDropdown />
+        </nav>
+  
+        {/* Pencarian */}
+        <div className="search-bar flex-grow flex items-center bg-white rounded-[15px] pl-3 pr-2 py-1 mx-auto w-full max-w-xl">
+          {/* Ikon Pencarian */}
+          <i className="fas fa-search text-gray-600 cursor-pointer text-2xl" onClick={handleSearchSubmit}></i>
+          {/* Input Pencarian */}
+          <input
+            type="text"
+            placeholder="Cari Produk..."
+            value={searchTerm}
+            onChange={handleSearchChange}
+            onKeyDown={(e) => e.key === "Enter" && handleSearchSubmit()} // Submit saat tekan Enter
+            className="search-input w-full px-2 py-1 rounded-[15px] focus:outline-none text-black"
+          />
+        </div>
+        
+        <nav className="flex space-x-4">
+          <Link to="/login" className="bg-red-300 text-black px-4 py-2 rounded-md hover:bg-red-400 transition">
+            Masuk
+          </Link>
+          <Link to="/register" className="bg-red-300 text-black px-4 py-2 rounded-md hover:bg-red-400 transition">
+            Daftar
+          </Link>
+        </nav>
+      </header>
+    );
+  }
 };
 
 export default Header;
